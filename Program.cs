829920fb4c092e3e5a6f6c0096b7f042e5c76b35
negin <neginkhosravi509@gmail.com>
2025-12-05 23:30:47 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mezrab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter one number");
+            int n = int.Parse(Console.ReadLine());
+            //چون باید کوچک تر از عدد دریافتی باشد
+            PMO3(n - 1);
+            Console.ReadKey();
+        }
+        static void PMO3(int n)
+        {
+            //شرط پایان بازگشت
+            if (n<=0)
+            return;
+            //اگر مضرب 3 بود چاپ کن 
+            if (n%3==0)
+            Console.WriteLine(n);
+            // بازگشت به عدد قبلی
+            PMO3(n - 1);
+        }
+    }
+}
